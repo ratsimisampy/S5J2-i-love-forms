4d46ad98d7780e72bc19adf2b0ba703757d606bb
Ratsimisampy <ratsimisampy@yahoo.fr>
2018-10-30 21:17:49 +0100

    2nd commit after creating db and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+    user = User.create!(username: Faker::Name.first_name, email: Faker::Internet.email ,bio: Faker::Lorem.sentence)
+end
