2173adb2a29a6d522f0ff232813d403f0087b8df
Ratsimisampy <ratsimisampy@yahoo.fr>
2018-10-30 21:32:22 +0100

    add def create content

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,12 @@
 class UserController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    #  1st form
+    User.create(username: params["username"], email: params["email"], bio: params["bio"])
+    puts params["username"]
+    puts "HELLLLLLOOOO HTML"
   end
 end
