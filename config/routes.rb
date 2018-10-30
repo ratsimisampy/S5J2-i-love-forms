60c2df002d5c9c613e4e66d2dbdd560e9dfc77fb
Ratsimisampy <ratsimisampy@yahoo.fr>
2018-10-30 23:42:41 +0100

    up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  root 'users#new'
+  root 'user#new'
 
   resources :users, only: [:new, :create]
-  
+
   get '/new', to: 'user#new'
   post '/new', to: 'user#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
