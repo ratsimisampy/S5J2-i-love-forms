6e51838078fddb97f5a8793ece4c654c3d9bf456
Ratsimisampy <ratsimisampy@yahoo.fr>
2018-10-30 21:21:21 +0100

    change urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/new', to: 'users#new'
-  post '/new', to: 'users#create'
+  get '/new', to: 'user#new'
+  post '/new', to: 'user#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
